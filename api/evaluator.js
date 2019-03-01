95d973aadc8a3a26e56fddba456b19aa56dc400c
Eugene Smirnov <zheka2304@gmail.com>
2019-03-01 22:32:49 +0300

    added material resources & boot sequence

--- api/evaluator.js
+++ api/evaluator.js
@@ -1,1 +1,20 @@
-alert("HELLO WORLD" + GIT_REPOSITORY_URL);
+var api_module_list = [];
+
+get_module_list(GIT_REPOSITORY_URL + API_MODULES_PATH).map(function(path) {
+	api_module_list[api_module_list.length] = new Executable({
+		url: GIT_REPOSITORY_URL + path
+	});
+});
+
+api_module_list.map(function(module) {
+	module.load();
+	if (module.exception != null) {
+		alert("error in module: " + module.exception);
+	}
+});
+
+var main_executable = new Executable({
+	url: GIT_REPOSITORY_URL + SOURCE_PATH
+});
+
+main_executable.load();
